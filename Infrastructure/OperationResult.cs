5f0b3c922dda5e1172d57a6dcbc759d407f6f836
Aragonro <Roket98@gmail.com>
2018-05-24 23:16:25 +0300

    Add files via upload

---
+++ Infrastructure/OperationResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductControl.BLL.Infrastructure
+{
+    public class OperationResult
+    {
+        public string Result { get; set; }
+
+        public OperationResult(string result)
+        {
+            Result = result;
+        }
+    }
+}
---
+++ Infrastructure/ServiceModule.cs
@@ -0,0 +1,24 @@
+﻿using Ninject.Modules;
+using ProductControl.Dal.Interfaces;
+using ProductControl.Dal.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductControl.BLL.Infrastructure
+{
+    public class ServiceModule : NinjectModule
+    {
+        private string connectionString;
+        public ServiceModule(string connection)
+        {
+            connectionString = connection;
+        }
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<EFUnitOfWork>().WithConstructorArgument(connectionString);
+        }
+    }
+}
