d933d0a7cbc41d82f266a999058e5d238afd2112
Aragonro <Roket98@gmail.com>
2018-05-24 23:17:22 +0300

    Add files via upload

---
+++ EF/ProductControlContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductControl.Dal.Entities;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ProductControl.Dal.EF
+{
+    public class ProductControlContext : IdentityDbContext<ApplicationUser>
+    {
+
+        public DbSet<Sensor> Sensors { get; set; }
+
+        public DbSet<Monitoring> Monitorings { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+        static ProductControlContext()
+        {
+            Database.SetInitializer(new ProductControlDbInitializer());
+        }
+
+        public ProductControlContext(string connectionString)
+            : base(connectionString)
+        {
+        }
+    }
+
+    public class ProductControlDbInitializer : DropCreateDatabaseIfModelChanges<ProductControlContext>
+    {
+        protected override void Seed(ProductControlContext db)
+        {
+            //db.Users.Add(new User { Login = "MyLogin", Password = "qweasd123", FirstName = "Ivan", SecondName = "Popovich" });
+            db.Sensors.Add(new Sensor { IsWorking = false, IsProduct = false });
+            db.SaveChanges();
+        }
+    }
+}
