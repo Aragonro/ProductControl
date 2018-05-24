a5b79f1b9ee2edddeb688e4f34f70465b4e8f49e
Aragonro <Roket98@gmail.com>
2018-05-24 23:19:08 +0300

    Add files via upload

---
+++ InterfacesDal/IUnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductControl.Dal.Entities;
+using ProductControl.Dal.Identity;
+
+namespace ProductControl.Dal.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ApplicationUserManager UserManager { get; }
+    
+        ApplicationRoleManager RoleManager { get; }
+
+        IRepository<Monitoring> Monitorings { get; }
+
+        IRepository<Product> Products { get; }
+
+        IRepository<Sensor> Sensors { get; }
+
+        IRepository<Order> Orders { get; }
+
+        void Save();
+    }
+}
