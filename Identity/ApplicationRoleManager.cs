8453055792accf1e6559e1247ebd83507a8b4f8a
Aragonro <Roket98@gmail.com>
2018-05-24 23:18:09 +0300

    Add files via upload

---
+++ Identity/ApplicationRoleManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductControl.Dal.Entities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace ProductControl.Dal.Identity
+{
+    public class ApplicationRoleManager : RoleManager<ApplicationRole>
+    {
+        public ApplicationRoleManager(RoleStore<ApplicationRole> store)
+                    : base(store)
+        { }
+    }
+}
