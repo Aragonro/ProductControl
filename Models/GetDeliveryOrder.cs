740e655a02543dc2aff4edf137b2b0930569ffcb
Aragonro <Roket98@gmail.com>
2018-05-28 00:39:00 +0300

    Add files via upload

---
+++ Models/GetDeliveryOrder.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProductControl.Models
+{
+    public class GetDeliveryOrder
+    {
+        public string DeliveryDate { get; set; }
+        public string DeliveryAddress { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
